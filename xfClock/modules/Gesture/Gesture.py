0c460748bb9ecbbfa70b9785c299d9f0bc0d9ec7
fhansa <fhan770@gmail.com>
2018-06-17 22:56:26 +0200

    fix

--- xfClock/modules/Gesture/Gesture.py
+++ xfClock/modules/Gesture/Gesture.py
@@ -61,28 +61,27 @@ class Gesture(xfClock.module.moduleBase):
 
         # Settings
         #apds.setProximityIntLowThreshold(50)
-
+        print("Starting gesture sensor")
         # Enable Gesture
-        if apds.enableGestureSensor():
+        apds.enableGestureSensor()
 
-            # Forever loop
-            try:
-                while True:
-                    sleep(0.25)
-                    if apds.isGestureAvailable():
-                        motion = apds.readGesture()
-                        gestval = dirs.get(motion, "unknown")
-                        #lock.aquire()
-                        #try:
-                        #    self.gesture = gestval
-                        #finally:
-                        #    lock.release()
+        # Forever loop
+        try:
+            while True:
+                sleep(0.25)
+                if apds.isGestureAvailable():
+                    motion = apds.readGesture()
+                    gestval = dirs.get(motion, "unknown")
+                    print("Gesture={}".format(dirs.get(motion, "unknown")))
+                    #lock.aquire()
+                    #try:
+                    #    self.gesture = gestval
+                    #finally:
+                    #    lock.release()
 
-                        mqttPublish.single("home/clock/gesture", gestval,  qos=0,hostname="home", port=1883, client_id="clock", auth={ "username":"fhan", "password":"194242!" })
-                        print("Gesture={}".format(dirs.get(motion, "unknown")))
+                    mqttPublish.single("home/clock/gesture", gestval,  qos=0,hostname="home", port=1883, client_id="clock", auth={ "username":"fhan", "password":"194242!" })
+                    print("Gesture={}".format(dirs.get(motion, "unknown")))
 
 
-            finally:
-                print("Leaving Gesture")
-        else:
-            print("Cannot enable Gesture sensor")
+        finally:
+            print("Leaving Gesture")
